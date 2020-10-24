93771c5d32689c749a33738f0acb606f0751d6bf
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2020-10-24 18:05:12 +1100

    Route Parameters

---
+++ BlazorProducts.Client/Pages/ReportError.razor.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class ReportError
+    {
+        [Parameter]
+        public int ErrorCode { get; set; }
+    
+        [Parameter]
+        public string ErrorDescription { get; set; }
+    }
+}
