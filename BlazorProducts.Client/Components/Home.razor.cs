106db9a584a57cf8ee8c5b4b296f7af5da7103c2
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2020-10-24 14:39:31 +1100

    Blazor Partial Classes

---
+++ BlazorProducts.Client/Components/Home.razor.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Components
+{
+    public partial class Home
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
+
+        [CascadingParameter(Name = "HeadingColor")]
+        public string Color { get; set; }
+    }
+}
