372a9b83447865168a7cd114f7c8f6eabe75480e
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2020-11-10 19:53:04 +1100

    Handling File Upload on the Server Side

---
+++ BlazorProducts.Server/BlazorProducts.Server/Controllers/UploadController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorProducts.Server.Controllers
+{
+    [Route("api/upload")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+
+                var folderName = Path.Combine("StaticFiles", "Images");
+
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                if (file.Length <= 0) return BadRequest();
+                
+                var name = ContentDispositionHeaderValue
+                    .Parse(file.ContentDisposition).FileName;
+                var fileName = name.Trim('"');
+                var fullPath = Path.Combine(pathToSave, fileName);
+                var dbPath = Path.Combine(folderName, fileName);
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return Ok(dbPath);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
